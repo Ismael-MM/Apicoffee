4ee8530174c188da12edf843ab85938c0b50cb80
ismael <ismaelmontelongomon@gmail.com>
2023-11-17 21:18:20 +0000

    A21 auth controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\ModuloController;
+use App\Http\Controllers\Auth\LoginRegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::resource('modulos', ModuloController::class);
+    Route::post('/logout', [LoginRegisterController::class, 'logout']);
+});
+
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
 });
