3123884eef3c7b655864a7f415abf068c64e6369
ismael <ismaelmontelongomon@gmail.com>
2023-11-17 19:26:41 +0000

    A21 actividad 2 esqueleto

---
+++ database/migrations/2023_11_17_190059_create_modulos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('modulos', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo')->unique();
+            $table->string('materia');
+            $table->integer('h_semanales');
+            $table->integer('h_totales');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('especialidad_id');
+            $table->unsignedBigInteger('curso_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('modulos');
+    }
+};
