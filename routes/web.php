294a3fd6757c20027d78d47056b74df29da4b97f
ismael <ismaelmontelongomon@gmail.com>
2023-12-01 21:34:50 +0000

    routa web prect

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/html/main', function (){
+        $pathToFile = public_path('html/main.html');
+        return response()->file($pathToFile);
+    });
+});
