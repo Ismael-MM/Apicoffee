66a8675a96c07ac2e2fccf7b76f0bf7730684a79
ismael <ismontelongo@outlook.com>
2023-12-09 17:11:54 +0000

    Add: nueva migracion para añadir 3 columnas a users

---
+++ database/migrations/2023_12_09_170540_modify_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('observaciones');
+            $table->string('horas_total');
+            $table->string('rol');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('observaciones');
+            $table->dropColumn('horas_total');
+            $table->dropColumn('rol');
+        });
+    }
+};
