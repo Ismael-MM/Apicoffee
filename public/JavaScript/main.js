1187755787b7c85c5eee2b2beebee2b65a88c1ec
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2023-12-15 21:07:07 +0000

    hola

--- public/JavaScript/main.js
+++ public/JavaScript/main.js
@@ -284,7 +284,7 @@ function rellenarModulosFormulario(moduloForm, datosModulos) {
             option.value = datos.id;
             option.textContent = datos.materia;
 
-            if (datos.user_id != null) {
+            if (datos.user_id == 0) {
                 option.disabled = true;
             }
             
