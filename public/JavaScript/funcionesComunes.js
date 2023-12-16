19b9049bd34e265e0e92f6401aa688fe48c47318
ismael <ismontelongo@outlook.com>
2023-12-16 15:35:30 +0000

    update: ahora se llama a la api para desconectar

--- public/JavaScript/funcionesComunes.js
+++ public/JavaScript/funcionesComunes.js
@@ -5,5 +5,15 @@ btn_logOut.addEventListener("click", logOut);
 function logOut() {
     sessionStorage.clear();
 
+    fetch('/api/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .catch(() => {
+            mostrarMensajeFlash()
+        });
+
     window.location.href = "/";
 }
